03fc90c07165f915c6c3f52b238ec97eace3ed10
bujosa <davidbujosa@gmail.com>
2023-05-08 22:20:15 -0400

    feat: add first example with macros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+macro_rules! pubsub_subscribe {
+    ($topic:expr) => {
+        pub fn subscribe_to_topic() {
+            // Aquí iría el código para conectarse al servicio de pubsub y suscribirse al tema especificado
+            println!("Subscribed to topic: {}", $topic);
+        }
+    };
 }
+
+pubsub_subscribe!("topic_1");
+fn main() {}

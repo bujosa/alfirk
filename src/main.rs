31aee3a8b8ad798fb92d151430f4e7d8cc9a43e6
bujosa <davidbujosa@gmail.com>
2023-05-11 19:06:03 -0400

    feat: add description to the first example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+// Example of a macro that receives an expression as a parameter
 macro_rules! pubsub_subscribe {
     ($topic:expr) => {
         pub fn subscribe_to_topic() {
-            // Aquí iría el código para conectarse al servicio de pubsub y suscribirse al tema especificado
             println!("Subscribed to topic: {}", $topic);
         }
     };
 }
 
 pubsub_subscribe!("topic_1");
-fn main() {}
+fn main() {
+    // Aquí iría el código para conectarse al servicio de pubsub y suscribirse al tema especificado
+    // println!("Subscribed to topic: {}", "topic_1");
+    subscribe_to_topic();
+}
